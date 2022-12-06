bf9ebdaaec189c0d312b8ce0fe2f259dbffc1d00
daniel cabral <danielxj6vs@gmail.com>
2022-12-06 19:32:27 -0300

    exercicio galeriareactjs v1

---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { api } from '../types/api';
+import { AlbumItem } from '../Components/AlbumItem';
+import { Album } from '../types/Album'
+
+export const Home = () => {
+    const [loading, setLoading] = useState(false);
+    const [list, setList] = useState<Album[]>([]);
+
+    useEffect(() => {
+        loadAlbums();
+    }, []);
+
+    const loadAlbums = async () => {
+        setLoading(true);
+        const albums = await api.getAlbums();
+        setList( albums );
+        setLoading(false);
+    }
+
+    return (
+        <div>
+            {loading && "Carregando..."}
+
+            {list.map((item, index) => (
+                <AlbumItem
+                    key={index}
+                    id={item.id}
+                    title={item.title}
+                />
+            ))}
+        </div>
+    );
+
+}
